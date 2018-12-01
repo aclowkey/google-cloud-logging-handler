41dc2bbd81f6e55cc49d25e972e74d4c7323bb73
Alex Chaplianka <alexettelis@gmail.com>
2018-12-01 15:07:10 +0200

    Adding the README as long description

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
 from setuptools import setup
 
+
+# read the contents of your README file
+from os import path
+from setuptools import setup
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='google_cloud_logging_handler',
     version='1.1',
     descrition='Logs directly to google cloud',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     license='MIT',
     author='Alex Chaplianka',
     url="https://github.com/aclowkey",
