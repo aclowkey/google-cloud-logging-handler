12751d36009b3ad91eddd7549dc27edffb3f5330
Alex Chaplianka <alexettelis@gmail.com>
2018-12-11 11:10:07 +0200

    Adding support to adding node's hostname

--- google_cloud_logging_handler/__init__.py
+++ google_cloud_logging_handler/__init__.py
@@ -1,4 +1,5 @@
 import json
+import socket
 from logging import StreamHandler
 from google.cloud import logging
 
@@ -18,14 +19,24 @@ class GoogleCloudHandler(StreamHandler):
             if not isinstance(parse_json, bool):
                 raise ValueError("Invalid value: parse_json must be of type bool")
             self.parse_json = parse_json
+        self.add_hostname = False
+        if 'add_hostname' in kwargs.keys():
+            add_hostname = kwargs['add_hostname']
+            if not isinstance(add_hostname, bool):
+                raise ValueError("Invalid value: parse_json must be of type bool")
+            self.add_hostname = add_hostname
+            self.hostname = socket.gethostname()
 
         self.logger = logging.Client().logger(log_name)
 
     def emit(self, record):
         text = self.format(record)
+        labels = {}
+        if self.add_hostname:
+            labels['host'] = self.hostname
         if self.parse_json:
             info = json.loads(text)
-            self.logger.log_struct(severity=record.levelname, info=info)
+            self.logger.log_struct(severity=record.levelname,info=info, labels=labels)
         else:
-            self.logger.log_text(severity=record.levelname, text=text)
+            self.logger.log_text(severity=record.levelname, text=text, labels=labels)
 
